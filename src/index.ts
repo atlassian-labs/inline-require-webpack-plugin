4e2f32e8beee7f1be27302fce4b84561d04327b6
Alberto Gasparin <agasparin@atlassian.com>
2022-02-13 09:58:00 +1100

    Better structure internals to allow future optimisations (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,11 @@
-import type { Compiler } from 'webpack';
 import webpack from 'webpack';
 import { RawSource, SourceMapSource, Source } from 'webpack-sources';
+import { processSource } from './processor';
+import type { SideEffectFree } from './types';
 
 const PLUGIN_NAME = 'InlineRequireWebpackPlugin';
 
-const webpackModuleHeader = '(function(module, __webpack_exports__, __webpack_require__) {';
-const importPattern =
-  /var (\w+_WEBPACK_[A-Z]+_MODULE_\w+) = ([/*#\w]*)(__webpack_require__[^;,]+);/g;
-
-const sideEffectFree = new Map<string | number, boolean>();
-
-function checkSideEffectFree(requireExpression: string) {
-  // check if module has sideEffects false
-  const [, importedModule] = requireExpression.match(/["']([^"']+)["']/) || [];
-  return sideEffectFree.get(importedModule) || false;
-}
-
-function collectRequires(src: string) {
-  // Collect require variables
-  const requireVariables = new Map<
-    string,
-    { variableName: string; requireExpression: string; isSideEffectFree: boolean }
-  >();
-
-  const matches = src.matchAll(importPattern);
-  for (const match of matches) {
-    const variableName = match[1];
-    let requireExpression = match[3];
-
-    // if referencing another require var, inline it
-    requireExpression = requireExpression.replace(
-      /\w+_WEBPACK_[A-Z]+_MODULE_\w+/,
-      (s) => requireVariables.get(s)?.requireExpression || s
-    );
-
-    requireVariables.set(variableName, {
-      variableName,
-      requireExpression,
-      isSideEffectFree: checkSideEffectFree(requireExpression),
-    });
-  }
-  return requireVariables;
-}
-
-function toString(input: string | ArrayBuffer) {
-  return typeof input === 'string'
-    ? input
-    : String.fromCharCode.apply(null, new Uint16Array(input) as any);
-}
+const excludeNull = Boolean as unknown as <T>(x: T | null) => x is T;
 
 export interface InlineRequireWebpackPluginOptions {
   sourceMap?: boolean;
@@ -56,111 +14,107 @@ export interface InlineRequireWebpackPluginOptions {
 class InlineRequireWebpackPlugin {
   private readonly options: InlineRequireWebpackPluginOptions;
 
+  private readonly sideEffectFree: SideEffectFree = new Map();
+
   constructor(options: Partial<InlineRequireWebpackPluginOptions> = {}) {
     this.options = { ...options };
   }
 
-  apply(compiler: Compiler) {
+  collectSideEffects(
+    compiler: webpack.Compiler,
+    compilation: webpack.compilation.Compilation,
+    modules: webpack.compilation.Module[]
+  ) {
+    for (const m of modules) {
+      // @ts-expect-error v5 only id getter
+      const id = compilation.chunkGraph ? compilation.chunkGraph.getModuleId(m) : m.id;
+      if (id != null && !this.sideEffectFree.has(id) && 'libIdent' in m) {
+        // @ts-expect-error libIdent missing in Module type
+        const ident: string = m.libIdent({
+          context: compiler.options.context,
+        });
+
+        // either the dependency has explicit sideEffect
+        // or we assume only local js/ts modules being sideEffect free
+        const isFree =
+          m.factoryMeta && m.factoryMeta.sideEffectFree != null
+            ? m.factoryMeta.sideEffectFree
+            : !ident.includes('node_modules') && /\.[jt]sx?$/.test(ident);
+
+        this.sideEffectFree.set(id, isFree);
+      }
+    }
+  }
+
+  async processFiles(
+    compiler: webpack.Compiler,
+    compilation: webpack.compilation.Compilation,
+    chunkFiles: string[]
+  ) {
     const sourceMap = this.options.sourceMap ?? !!compiler.options.devtool;
 
+    const chunkAssets: string[] = Array.from(compilation.additionalChunkAssets || []);
+    const files = [...chunkFiles, ...chunkAssets];
+
+    const processed = files.map((file) => {
+      // skip non-JS files
+      if (!file.match(/\.m?[jt]sx?$/i)) return null;
+
+      const asset: Source = compilation.assets[file];
+
+      const original =
+        sourceMap && asset.sourceAndMap
+          ? asset.sourceAndMap()
+          : {
+              source: asset.source() as string,
+              map: sourceMap && typeof asset.map === 'function' ? asset.map() : null,
+            };
+
+      const result = processSource(file, original, this.sideEffectFree);
+
+      return {
+        file,
+        asset:
+          result.map && original.map
+            ? new SourceMapSource(result.source, file, result.map, original.source, original.map)
+            : new RawSource(result.source),
+      };
+    });
+
+    processed.filter(excludeNull).forEach(({ file, asset }) => {
+      compilation.assets[file] = asset;
+    });
+  }
+
+  apply(compiler: webpack.Compiler) {
     compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
-      function collectSideEffects(modules: webpack.compilation.Module[]) {
-        for (const m of modules) {
-          // @ts-expect-error v5 only id getter
-          const id = compilation.chunkGraph ? compilation.chunkGraph.getModuleId(m) : m.id;
-          if (id != null && !sideEffectFree.has(id) && 'libIdent' in m) {
-            const ident: string = (m as any).libIdent({
-              context: compiler.options.context,
-            });
-
-            // either the dependency has explicit sideEffect
-            // or we assume only local js/ts modules being sideEffect free
-            const isFree =
-              m.factoryMeta && m.factoryMeta.sideEffectFree != null
-                ? m.factoryMeta.sideEffectFree
-                : !ident.includes('node_modules') && /\.[jt]sx?$/.test(ident);
-
-            sideEffectFree.set(id, isFree);
-          }
-        }
-      }
-
-      compilation.hooks.afterOptimizeModuleIds.tap(PLUGIN_NAME, collectSideEffects);
-
-      function manipulateAsset(old: Source, file: string) {
-        const sourceAndMap =
-          sourceMap && old.sourceAndMap
-            ? old.sourceAndMap()
-            : {
-                source: old.source(),
-                map: sourceMap && typeof old.map === 'function' ? old.map() : null,
-              };
-
-        const src = toString(sourceAndMap.source)
-          .split(webpackModuleHeader)
-          .map((v) => {
-            // Collect require variables
-            const requireVariables = collectRequires(v);
-            let output = v;
-
-            // Replace variable names.
-            for (const [
-              variableName,
-              { requireExpression, isSideEffectFree },
-            ] of requireVariables.entries()) {
-              // eslint-disable-next-line no-continue
-              if (!isSideEffectFree) continue;
-
-              // strip top level var declarations
-              const declarationlessOutput = output.replace(
-                new RegExp(`var ${variableName}[^\\w]([^;]+);`),
-                (m, p0) => `// (inlined) ${(p0.match(/"([^"]+)/) || [])[1]}`
-              );
-
-              // replace inline variable references with require expression
-              const reflessOutput = declarationlessOutput.replace(
-                new RegExp(`([^\\w])${variableName}([^\\w])`, 'g'),
-                `$1(${requireExpression})$2`
-              );
-
-              if (reflessOutput !== declarationlessOutput) {
-                // import var is being used somewhere, confirm replacements
-                output = reflessOutput;
-              }
-            }
-
-            return output;
-          })
-          .join(webpackModuleHeader);
-
-        return sourceAndMap.map
-          ? new SourceMapSource(src, file, sourceAndMap.map)
-          : new RawSource(src);
-      }
+      compilation.hooks.afterOptimizeModuleIds.tap(
+        PLUGIN_NAME,
+        this.collectSideEffects.bind(this, compiler, compilation)
+      );
 
       // Webpack v5 hook (as optimizeChunkAssets is deprecated)
       if ('processAssets' in compilation.hooks) {
         // @ts-expect-error v5 only hook
-        compilation.hooks.processAssets.tap(
+        compilation.hooks.processAssets.tapPromise(
           {
             name: PLUGIN_NAME,
             // @ts-expect-error v5 only const
-            stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_COUNT,
+            stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
           },
           (assets: Record<string, Source>) => {
-            Object.keys(assets).forEach((file) => {
-              compilation.updateAsset(file, (old) => manipulateAsset(old, file));
-            });
+            const chunkFiles = Object.keys(assets);
+            return this.processFiles(compiler, compilation, chunkFiles);
           }
         );
       } else {
         // Webpack v4 hook
-        compilation.hooks.optimizeChunkAssets.tap(PLUGIN_NAME, (chunks) => {
-          for (const chunk of chunks) {
-            for (const file of chunk.files) {
-              compilation.updateAsset(file, (old) => manipulateAsset(old, file));
-            }
-          }
+        compilation.hooks.optimizeChunkAssets.tapPromise(PLUGIN_NAME, (chunks) => {
+          const chunkFiles = Array.from(chunks).reduce(
+            (acc, chunk) => acc.concat(Array.from(chunk.files || [])),
+            [] as string[]
+          );
+          return this.processFiles(compiler, compilation, chunkFiles);
         });
       }
     });
---
+++ src/processor.ts
@@ -0,0 +1,81 @@
+import type { SourceAndMapResult } from 'webpack-sources';
+import type { SideEffectFree } from './types';
+
+const webpackModuleHeader = '(function(module, __webpack_exports__, __webpack_require__) {';
+const importPattern =
+  /var (\w+_WEBPACK_[A-Z]+_MODULE_\w+) = ([/*#\w]*)(__webpack_require__[^;,]+);/g;
+
+function checkSideEffectFree(sideEffectFree: SideEffectFree, requireExpression: string) {
+  // check if module has sideEffects false
+  const [, importedModule] = requireExpression.match(/["']([^"']+)["']/) || [];
+  return sideEffectFree.get(importedModule) || false;
+}
+
+function collectRequires(src: string, sideEffectFree: SideEffectFree) {
+  // Collect require variables
+  const requireVariables = new Map();
+
+  const matches = src.matchAll(importPattern);
+  for (const match of matches) {
+    const variableName = match[1];
+    let requireExpression = match[3];
+
+    // if referencing another require var, inline it
+    requireExpression = requireExpression.replace(
+      /\w+_WEBPACK_[A-Z]+_MODULE_\w+/,
+      (s) => (requireVariables.get(s) && requireVariables.get(s).requireExpression) || s
+    );
+
+    requireVariables.set(variableName, {
+      variableName,
+      requireExpression,
+      isSideEffectFree: checkSideEffectFree(sideEffectFree, requireExpression),
+    });
+  }
+  return requireVariables;
+}
+
+export function processSource(
+  file: string,
+  original: SourceAndMapResult,
+  sideEffectFree: SideEffectFree
+): SourceAndMapResult {
+  const src = original.source
+    .split(webpackModuleHeader)
+    .map((v) => {
+      // Collect require variables
+      const requireVariables = collectRequires(v, sideEffectFree);
+      let output = v;
+
+      // Replace variable names.
+      for (const [
+        variableName,
+        { requireExpression, isSideEffectFree },
+      ] of requireVariables.entries()) {
+        // eslint-disable-next-line no-continue
+        if (!isSideEffectFree) continue;
+
+        // strip top level var declarations
+        const declarationlessOutput = output.replace(
+          new RegExp(`var ${variableName}[^\\w]([^;]+);`),
+          (m, p0) => `// (inlined) ${(p0.match(/"([^"]+)/) || [])[1]}`
+        );
+
+        // replace inline variable references with require expression
+        const reflessOutput = declarationlessOutput.replace(
+          new RegExp(`([^\\w])${variableName}([^\\w])`, 'g'),
+          `$1(${requireExpression})$2`
+        );
+
+        if (reflessOutput !== declarationlessOutput) {
+          // import var is being used somewhere, confirm replacements
+          output = reflessOutput;
+        }
+      }
+
+      return output;
+    })
+    .join(webpackModuleHeader);
+
+  return { source: src, map: original.map };
+}
